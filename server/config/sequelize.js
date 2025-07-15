8f3604c612e57124c3eeb8480a3cbc8477ca8331
prajjwal <prajjwal6464@gmail.com>
2025-07-15 16:50:49 +0545

    fix: abstracted setups to .env

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('notesapp', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false, 
+  logging: false,  
 });
 
 module.exports = sequelize;
+
